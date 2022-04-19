36ceab35544c746091a0b00290a941251adc9af0
Jaime <jaume.lan@gmail.com>
2022-04-19 07:07:05 -0300

    sign in route

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -11,9 +11,12 @@ const Navigation = () => {
                     <ShopLogo className="logo" />
                 </Link>
 
-                <div className="links-container">
+                <div className="nav-links-container">
                     <Link className="nav-link" to="/shop">
                         SHOP
+                    </Link>
+                    <Link className="nav-link" to="/signin">
+                        SIGN IN
                     </Link>
                 </div>
             </div>
